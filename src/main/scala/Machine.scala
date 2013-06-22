f71e57058269ae9e46405c62e2dbf0b9240e5154
Chris Lewis <clewis@novus.com>
2013-06-22 13:37:31 -0400

    extracted Semantics strategies; Machine folds over expressions and statements to generalize accumulation of evaluated results

--- src/main/scala/Machine.scala
+++ src/main/scala/Machine.scala
@@ -1,73 +1,43 @@
 package understandingcomputation.simple
 
-object Machine {
-
-  /* An environment is a table of identifiers to expressions. As there is no
+object `package` {
+    /* An environment is a table of identifiers to expressions. As there is no
      expression type to represent irreducible values, an identifier can bind to
      a reducible expression or a fully reduced primitive. */
   type Environment = Map[Symbol, Expression]
 
-  def reduceExpression(expr: Expression, env: Environment): Expression =
-    expr match {
-      case n @ Number(_) => n
-
-      case Variable(n) => env(n)
-
-      case Add(Reducible(l), r) => Add(reduceExpression(l, env), r)
-      case Add(l, Reducible(r)) => Add(l, reduceExpression(r, env))
-      case Add(Number(l), Number(r)) => Number(l + r)
-
-      case Multiply(Reducible(l), r) => Multiply(reduceExpression(l, env), r)
-      case Multiply(l, Reducible(r)) => Multiply(l, reduceExpression(r, env))
-      case Multiply(Number(l), Number(r)) => Number(l * r)
-
-      case LessThan(Reducible(l), r) => LessThan(reduceExpression(l, env), r)
-      case LessThan(l, Reducible(r)) => LessThan(l, reduceExpression(r, env))
-      case LessThan(Number(l), Number(r)) => Bool(l < r)
-    }
-
-  def reduceStatement(stmt: Statement, env: Environment): (Statement, Environment) =
-    stmt match {
-      case Assign(n, Reducible(expr)) => Assign(n, reduceExpression(expr, env)) -> env
-      case Assign(n, expr) => DoNothing -> (env + (n -> expr))
-
-      case If(Reducible(expr), c, a) => reduceStatement(If(reduceExpression(expr, env), c, a), env)
-      case If(Bool(true), c, _) => c -> env
-      case If(Bool(false), _, a) => a -> env
-
-      case Sequence(DoNothing, s) => (s, env)
-      case Sequence(f, s) =>
-        val (rf, renv) = reduceStatement(f, env)
-        (Sequence(rf, s), renv)
-
-      case _ => (stmt, env)
-    }
-
-  def runExpression(expr: Expression, env: Environment) {
-    println(expr)
-    expr match {
-      case Reducible(ee) => runExpression(reduceExpression(ee, env), env)
-      case _ => ()
-    }
+  /* A Convenient extractor to test for reducible expressions. */
+  object Reducible {
+    def unapply(e: Expression) =
+      e match {
+        case Bool(_) | Number(_) => None
+        case _ => Some(e)
+      }
   }
-
-  def runStatement(stmt: Statement, env: Environment) {
-    println(stmt + " ---- " + env)
-    stmt match {
-      case DoNothing => ()
-      case s =>
-        val (rs, renv) = reduceStatement(s, env)
-        runStatement(rs, renv)
-    }
-  }
-
 }
 
-/* A Convenient extractor to test for reducible expressions. */
-object Reducible {
-  def unapply(e: Expression) =
-    e match {
-      case Bool(_) | Number(_) => None
-      case _ => Some(e)
+object Machine {
+
+  def foldExpression[A](sem: Semantics, expr: Expression, env: Environment)(zero: A)(op: (Expression, A) => A): A =
+    expr match {
+      case Reducible(ee) =>
+        val rexpr = sem.reduceExpression(ee, env)
+        foldExpression(sem, rexpr, env)(op(rexpr, zero))(op)
+      case _ => zero
     }
+
+  def foldStatement[A](sem: Semantics, stmt: Statement, env: Environment)(zero: A)(op: (Statement, Environment, A) => A): A =
+    stmt match {
+      case DoNothing => zero
+      case s =>
+        val (rs, renv) = sem.reduceStatement(s, env)
+        foldStatement(sem, rs, renv)(op(rs, renv, zero))(op) // TODO will miss the first
+    }
+
+  def runExpression(sem: Semantics, expr: Expression, env: Environment) =
+    foldExpression(sem, expr, env)(()) { (e, _) => println(e) }
+
+  def runStatement(sem: Semantics, stmt: Statement, env: Environment) =
+    foldStatement(sem, stmt, env)(()) { (stmt, env, _) => println(stmt + " ---- " + env) }
+
 }
--- src/main/scala/Statement.scala
+++ src/main/scala/Statement.scala
@@ -4,10 +4,16 @@ package understandingcomputation.simple
    could be forced under the Expression tree with the introduction of a Unit
    primative, but for now we'll keep with the book and keep them separated. */
 sealed trait Statement
-case object DoNothing extends Statement
 
+/* A pair of statements. */
+final case class Sequence(first: Statement, second: Statement) extends Statement
+
+/* An assigmnet binds an identifier to an expression when evaluated in an environment. */
 final case class Assign(name: Symbol, expr: Expression) extends Statement
 
+/* Constrol structures. */
 final case class If(condition: Expression, consequence: Statement, alternative: Statement) extends Statement
+final case class While(condition: Expression, stmt: Statement) extends Statement
 
-final case class Sequence(first: Statement, second: Statement) extends Statement
+/* A bottom value for statements indicating no further evaluation is possible. */
+case object DoNothing extends Statement
