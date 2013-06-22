a5f1fe0d23df516956d1ec3ec7132c8b09fbe307
Chris Lewis <clewis@novus.com>
2013-06-22 13:58:08 -0400

    pkg object externalized

--- src/main/scala/Machine.scala
+++ src/main/scala/Machine.scala
@@ -1,21 +1,5 @@
 package understandingcomputation.simple
 
-object `package` {
-    /* An environment is a table of identifiers to expressions. As there is no
-     expression type to represent irreducible values, an identifier can bind to
-     a reducible expression or a fully reduced primitive. */
-  type Environment = Map[Symbol, Expression]
-
-  /* A Convenient extractor to test for reducible expressions. */
-  object Reducible {
-    def unapply(e: Expression) =
-      e match {
-        case Bool(_) | Number(_) => None
-        case _ => Some(e)
-      }
-  }
-}
-
 object Machine {
 
   def foldExpression[A](sem: Semantics, expr: Expression, env: Environment)(zero: A)(op: (Expression, A) => A): A =
