0c5e79bbec9fed5bfd0dcb5da295e961667c6bb7
Chris Lewis <clewis@novus.com>
2013-06-23 14:42:27 -0400

    streamline Semantics trait; Machine conveniences

--- src/main/scala/Machine.scala
+++ src/main/scala/Machine.scala
@@ -1,27 +1,25 @@
 package understandingcomputation.simple
 
+class Machine(sem: Semantics) {
+
+  def runExpression(expr: Expression, env: Environment) =
+    Machine.runExpression(sem, expr, env)
+
+  def runStatement(stmt: Statement, env: Environment) =
+    Machine.runStatement(sem, stmt, env)
+
+}
+
 object Machine {
 
-  def foldExpression[A](sem: Semantics, expr: Expression, env: Environment)(zero: A)(op: (Expression, A) => A): A =
-    expr match {
-      case Reducible(ee) =>
-        val rexpr = sem.reduceExpression(ee, env)
-        foldExpression(sem, rexpr, env)(op(rexpr, zero))(op)
-      case _ => zero
-    }
-
-  def foldStatement[A](sem: Semantics, stmt: Statement, env: Environment)(zero: A)(op: (Statement, Environment, A) => A): A =
-    stmt match {
-      case DoNothing => zero
-      case s =>
-        val (rs, renv) = sem.reduceStatement(s, env)
-        foldStatement(sem, rs, renv)(op(rs, renv, zero))(op) // TODO will miss the first
-    }
-
+  def apply(sem: Semantics) = new Machine(sem)
+  
   def runExpression(sem: Semantics, expr: Expression, env: Environment) =
-    foldExpression(sem, expr, env)(()) { (e, _) => println(e) }
+    println(sem.evaluateExpression(expr, env))
 
-  def runStatement(sem: Semantics, stmt: Statement, env: Environment) =
-    foldStatement(sem, stmt, env)(()) { (stmt, env, _) => println(stmt + " ---- " + env) }
+  def runStatement(sem: Semantics, stmt: Statement, env: Environment) = {
+    val (rs, renv) = sem.evaluateStatement(stmt, env)
+    println(rs + " ---- " + renv)
+  }
 
 }
--- src/main/scala/Semantics.scala
+++ src/main/scala/Semantics.scala
@@ -2,13 +2,46 @@ package understandingcomputation.simple
 
 /* A semantics implements an interpretation strategy. */
 sealed trait Semantics {
-  def reduceExpression(expr: Expression, env: Environment): Expression
-  def reduceStatement(stmt: Statement, env: Environment): (Statement, Environment)
+
+  /** Evaluate an expression to completion. */
+  def evaluateExpression(expr: Expression, env: Environment): Expression
+
+  /** Evaluate a statement to completion. */
+  def evaluateStatement(stmt: Statement, env: Environment): (Statement, Environment)
+
 }
 
 object SmallStep extends Semantics {
 
-  override def reduceExpression(expr: Expression, env: Environment): Expression =
+  override def evaluateExpression(expr: Expression, env: Environment): Expression =
+    exprReduction(expr, env).head
+
+  override def evaluateStatement(stmt: Statement, env: Environment): (Statement, Environment) =
+    stmtReduction(stmt, env).head
+
+  val exprReduction: (Expression, Environment) => List[Expression] =
+    foldExpression(_, _)(List[Expression]())(_ :: _)
+
+  val stmtReduction: (Statement, Environment) => List[(Statement, Environment)] =
+    foldStatement(_, _)(List[(Statement, Environment)]())(_ :: _)
+
+  def foldExpression[A](expr: Expression, env: Environment)(zero: A)(op: (Expression, A) => A): A =
+    expr match {
+      case Reducible(ee) =>
+        val rexpr = reduceExpression(ee, env)
+        foldExpression(rexpr, env)(op(rexpr, zero))(op)
+      case _ => zero
+    }
+
+  def foldStatement[A](stmt: Statement, env: Environment)(zero: A)(op: ((Statement, Environment), A) => A): A =
+    stmt match {
+      case DoNothing => zero
+      case s =>
+        val (rs, renv) = reduceStatement(s, env)
+        foldStatement(rs, renv)(op((rs, renv), zero))(op)
+    }
+
+  def reduceExpression(expr: Expression, env: Environment): Expression =
     expr match {
       case n @ Number(_) => n
 
@@ -16,34 +49,34 @@ object SmallStep extends Semantics {
 
       case Variable(n) => env(n)
 
-      case Add(Reducible(l), r) => Add(reduceExpression(l, env), r)
-      case Add(l, Reducible(r)) => Add(l, reduceExpression(r, env))
+      case Add(Reducible(l), r) => Add(evaluateExpression(l, env), r)
+      case Add(l, Reducible(r)) => Add(l, evaluateExpression(r, env))
       case Add(Number(l), Number(r)) => Number(l + r)
 
-      case Multiply(Reducible(l), r) => Multiply(reduceExpression(l, env), r)
-      case Multiply(l, Reducible(r)) => Multiply(l, reduceExpression(r, env))
+      case Multiply(Reducible(l), r) => Multiply(evaluateExpression(l, env), r)
+      case Multiply(l, Reducible(r)) => Multiply(l, evaluateExpression(r, env))
       case Multiply(Number(l), Number(r)) => Number(l * r)
 
-      case LessThan(Reducible(l), r) => LessThan(reduceExpression(l, env), r)
-      case LessThan(l, Reducible(r)) => LessThan(l, reduceExpression(r, env))
+      case LessThan(Reducible(l), r) => LessThan(evaluateExpression(l, env), r)
+      case LessThan(l, Reducible(r)) => LessThan(l, evaluateExpression(r, env))
       case LessThan(Number(l), Number(r)) => Bool(l < r)
     }
 
-  override def reduceStatement(stmt: Statement, env: Environment): (Statement, Environment) =
+  def reduceStatement(stmt: Statement, env: Environment): (Statement, Environment) =
     stmt match {
-      case Assign(n, Reducible(expr)) => Assign(n, reduceExpression(expr, env)) -> env
+      case Assign(n, Reducible(expr)) => Assign(n, evaluateExpression(expr, env)) -> env
       case Assign(n, expr) => DoNothing -> (env + (n -> expr))
 
-      case If(Reducible(expr), c, a) => reduceStatement(If(reduceExpression(expr, env), c, a), env)
+      case If(Reducible(expr), c, a) => evaluateStatement(If(evaluateExpression(expr, env), c, a), env)
       case If(Bool(true), c, _) => c -> env
       case If(Bool(false), _, a) => a -> env
 
       case Sequence(DoNothing, s) => (s, env)
       case Sequence(f, s) =>
-        val (rf, renv) = reduceStatement(f, env)
+        val (rf, renv) = evaluateStatement(f, env)
         (Sequence(rf, s), renv)
 
-      case While(e, s) => reduceStatement(If(e, Sequence(s, While(e, s)), DoNothing), env)
+      case While(e, s) => evaluateStatement(If(e, Sequence(s, While(e, s)), DoNothing), env)
 
       case _ => (stmt, env)
     }
@@ -53,9 +86,9 @@ object BigStep extends Semantics {
 
   /* Down cast the result of a reduction to a specific expression type. */
   private def evalExprAs[E <: Expression](expr: Expression, env: Environment): E =
-    reduceExpression(expr, env).asInstanceOf[E]
+    evaluateExpression(expr, env).asInstanceOf[E]
 
-  override def reduceExpression(expr: Expression, env: Environment): Expression =
+  override def evaluateExpression(expr: Expression, env: Environment): Expression =
     expr match {
       case b @ Bool(_) => b
 
@@ -71,26 +104,25 @@ object BigStep extends Semantics {
 
     }
 
-  override def reduceStatement(stmt: Statement, env: Environment): (Statement, Environment) =
+  override def evaluateStatement(stmt: Statement, env: Environment): (Statement, Environment) =
     stmt match {
-      case Assign(n, expr) => DoNothing -> (env + (n -> reduceExpression(expr, env)))
+      case Assign(n, expr) => DoNothing -> (env + (n -> evaluateExpression(expr, env)))
 
       case If(cond, c, a) =>
-        if(reduceExpression(cond, env) == Bool(true)) reduceStatement(c, env)
-        else reduceStatement(a, env)
+        if(evaluateExpression(cond, env) == Bool(true)) evaluateStatement(c, env)
+        else evaluateStatement(a, env)
 
       case Sequence(f, s) =>
-        val (rf, renv) = reduceStatement(f, env)
-        reduceStatement(s, renv)
+        val (rf, renv) = evaluateStatement(f, env)
+        evaluateStatement(s, renv)
 
       case While(c, s) =>
-        if(reduceExpression(c, env) == Bool(true)) {
-          val (_, renv) = reduceStatement(s, env)
-          reduceStatement(While(c, s), renv)
+        if(evaluateExpression(c, env) == Bool(true)) {
+          val (_, renv) = evaluateStatement(s, env)
+          evaluateStatement(While(c, s), renv)
         } else s -> env
 
       case DoNothing => DoNothing -> env
     }
 
 }
-
